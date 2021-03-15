bf9357939c3884700e1cd2ec68776077dfddee73
Florian Chappaz <florian.chappaz74@gmail.com>
2021-03-15 18:33:44 +0100

    TP3 Q7

--- Exercices/TP3/collection_distribution_search_and_query.php
+++ Exercices/TP3/collection_distribution_search_and_query.php
@@ -35,12 +35,43 @@ include_once('tp3-helpers.php');
                 $api_response_movies_array = json_decode($api_response_movies);
                 echo "<ul>";
 
+                $collection_credits = array(); // Stocke tous les acteurs de la collection : name => [Objet représentant l'acteur]
+
                 foreach($api_response_movies_array->{'parts'} as $movie) {
                   echo "<li><h3>".$movie->{'title'}."</h3>";
                   echo "<p><b>ID : </b>".$movie->{'id'}."<br>";
-                  echo "<b>Date de sortie : </b>".$movie->{'release_date'}."</p></li>";
+                  echo "<b>Release date : </b>".$movie->{'release_date'}."</p>";
+
+                  // Pour chaque film, on récupère ses crédits
+                  $url_component = "movie/".$movie->{'id'}."/credits";
+                  $api_response_credits = tmdbget($url_component);
+                  $credits_array = json_decode($api_response_credits)->{'cast'};
+                  
+                  // On parcours chaque participant, en ne traitant que les acteurs (ceux ayant une propriété character)
+                  foreach ($credits_array as $people) {
+                    if (property_exists($people, "character")) {
+                      $url_component = "person/".$people->{'id'}."/movie_credits";
+                      $people_array = json_decode(tmdbget($url_component));
+                      $people_name = $people->{'name'};
+                      $character = $people->{'character'};
+                      if (!array_key_exists($people_name, $collection_credits)) {
+                        $collection_credits[$people_name] = array($character, $people_array);
+                      }
+                    }
+                  }
+
+                  echo "</li>";
                 }
 
+                echo "</ul>";
+
+                echo "<h3>Collection credits</h3>";
+                echo "<ul>";
+                foreach ($collection_credits as $actor_name=>$infos_array) {
+                  echo "<li><p>";
+                  echo "<b>".$actor_name." : </b>Role=".$infos_array[0].", Number of movies=".count($infos_array[1]->{'cast'});
+                  echo "</p></li>";
+                }
                 echo "</ul>";
             }
         }
